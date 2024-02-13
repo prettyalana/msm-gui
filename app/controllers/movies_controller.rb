0dba550c15f46729e979355e9616268f2e541118
Alana Edwards <129349619+prettyalana@users.noreply.github.com>
2024-02-13 03:14:09 +0000

    First commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,17 @@
 class MoviesController < ApplicationController
+  def create
+    new_movie = Movie.new
+    new_movie.title = params.fetch("movie_title")
+    new_movie.year = params.fetch("movie_year")
+    new_movie.duration = params.fetch("movie_duration")
+    new_movie.description = params.fetch("movie_description")
+    new_movie.image = params.fetch("movie_image")
+    new_movie.director_id = params.fetch("movie_director_id")
+
+    new_movie.save 
+
+    redirect_to("/movies")
+  end
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
